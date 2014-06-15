ca112e7e4cc3366452528815d1cf7ad4a50e1adb
Brian Cline <brian.cline@gmail.com>
2014-06-15 05:22:02 -0500

    No More Trailing Spaces™

--- process.php
+++ process.php
@@ -1,13 +1,13 @@
 <?php
-	
+
 	$dropFirst = true;
 
 	$countriesSource = 'http://git.debian.org/?p=iso-codes/iso-codes.git;a=blob_plain;f=iso_3166/iso_3166.xml;hb=HEAD';
 	$provincesSource = 'http://git.debian.org/?p=iso-codes/iso-codes.git;a=blob_plain;f=iso_3166_2/iso_3166_2.xml;hb=HEAD';
-	
+
 	$co = getCountries($countriesSource);
 	$pr = getProvinces($provincesSource);
-	
+
 	if($dropFirst)
 	{
 		printf("DROP TABLE IF EXISTS `meta_country`;\n");
@@ -23,9 +23,9 @@
 				PRIMARY KEY (`alpha_2`),
 				INDEX (`alpha_3`)
 			)
-			ENGINE=MYISAM 
-			CHARACTER SET utf8 
-			COLLATE utf8_unicode_ci 
+			ENGINE=MYISAM
+			CHARACTER SET utf8
+			COLLATE utf8_unicode_ci
 			COMMENT='ISO 3166 Country List';
 		");
 		printf("
@@ -42,9 +42,9 @@
 				INDEX (`iso_code`),
 				INDEX (`country_code`)
 			)
-			ENGINE=MYISAM 
-			CHARACTER SET utf8 
-			COLLATE utf8_unicode_ci 
+			ENGINE=MYISAM
+			CHARACTER SET utf8
+			COLLATE utf8_unicode_ci
 			COMMENT='ISO 3166-2 Administrative Entity List';
 		");
 
@@ -54,18 +54,18 @@
 		printf("TRUNCATE `meta_country`;\n");
 		printf("TRUNCATE `meta_province`;\n");
 	}
-	
+
 	printf("\n");
-	
-	
+
+
 	$parentCodes = array();
 	foreach($co as $country) {
 		$coCode = $country['alpha_2'];
-		
+
 		foreach($country as $key => $val) {
 			$country[$key] = mysql_real_escape_string($val);
 		}
-		
+
 		printf("\nINSERT INTO `meta_country` (`alpha_2`, `alpha_3`, `numeric`, `name`, `common_name`, `official_name`)\n");
 		printf("VALUES ('%s', '%s', '%s', '%s', '%s', '%s');\n",
 			$country['alpha_2'],
@@ -75,7 +75,7 @@
 			$country['common_name'],
 			$country['official_name']
 		);
-		
+
 		if(!array_key_exists($coCode, $pr)) {
 			continue;
 		}
@@ -83,14 +83,14 @@
 		if (!empty($pr[$coCode])) {
 			printf("INSERT INTO `meta_province` (`sys_code`, `iso_code`, `country_code`, `code`, `name`, `type`, `parent`) VALUES\n");
 		}
-		
+
 		for($i = 0; $i < count($pr[$coCode]); $i++) {
 			$province = $pr[$coCode][$i];
-			
+
 			foreach($province as $key => $value) {
 				$province[$key] = mysql_real_escape_string($value);
 			}
-			
+
 			printf("('%s', '%s', '%s', '%s', '%s', '%s', '%s')%s\n",
 				$province['sys_code'],
 				$province['iso_code'],
@@ -101,7 +101,7 @@
 				$province['parent'],
 				($i < count($pr[$coCode]) - 1) ? ',' : ';'
 			);
-			
+
 			if (!empty($province['parent'])) {
 				$parentCodes[] = $province['parent'];
 			}
@@ -110,7 +110,7 @@
 
 	printf("\n\nUPDATE `meta_province` SET `is_parent` = '1' WHERE `code` IN ('%s');\n", implode("','", $parentCodes));
 
-	
+
 	function getCountries($fn) {
 		$co = array();
 		$raw = file_get_contents($fn);
@@ -118,7 +118,7 @@
 
 		foreach($x->xpath('//iso_3166_entry') as $e) {
 			$countryCode = (string) $e['alpha_2_code'];
-			
+
 			$co[$countryCode] = array(
 				'alpha_2'       => $countryCode,
 				'alpha_3'       => (string) $e['alpha_3_code'],
@@ -128,40 +128,40 @@
 				'official_name' => (string) $e['official_name']
 			);
 		}
-		
+
 		return $co;
 	}
-	
+
 	function getProvinces($fn) {
 		$co = array();
 		$raw = file_get_contents($fn);
 		$x = new SimpleXMLElement($raw);
 		$sysCodes = array();
-		
+
 		foreach($x->xpath('//iso_3166_country') as $country) {
 			$countryCode = (string) $country['code'];
-			
+
 			$subsets = $country->xpath('iso_3166_subset');
 			foreach($subsets as $subset) {
 				$subsetType = (string) $subset['type'];
 				$entries = $subset->xpath('iso_3166_2_entry');
-				
+
 				foreach($entries as $entry) {
 					$sysCodeNum = 1;
 					$isoCode = (string) $entry['code'];
 					$sysCode = $isoCode;
 					$parent = (string) $entry['parent'];
 					$entryCode = substr($isoCode, strlen($countryCode) + 1); // Skip 'XX-' for the code
-					
+
 					if (!empty($parent)) {
 						$sysCode = $countryCode .'-'. $parent .'-'. $entryCode;
 						$parent = $countryCode .'-'. $parent;
 					}
-					
+
 					while (isset($sysCodes[$sysCode])) {
 						$sysCode = $isoCode .'-X'. ++$sysCodeNum;
 					}
-					
+
 					$sysCodes[$sysCode] = 0;
 					$co[$countryCode][] = array(
 						'sys_code'      => $sysCode,
@@ -175,7 +175,6 @@
 				}
 			}
 		}
-		
+
 		return $co;
 	}
-
